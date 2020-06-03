ef98639a7f318d4223a760dbad82389335f94e7b
angeloevangelista <angeloevan.ane@gmail.com>
2020-06-03 20:24:58 -0300

    Importado PointController

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 
 import ItemController from './controllers/ItemController';
+import PointController from './controllers/PointController';
 
 const routes = Router();
 
+/**
+ * Items
+ */
 routes.get('/items', ItemController.index);
+
+/**
+ * Points
+ */
+routes.get('/points', PointController.index);
+
+routes.post('/points', PointController.create);
 
 export default routes;
