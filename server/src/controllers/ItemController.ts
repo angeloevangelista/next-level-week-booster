005e06453f3b19d94cbe7ccd0af1eef061eb05f5
angeloevangelista <angeloevan.ane@gmail.com>
2020-06-03 16:30:53 -0300

    Rota de listagem de items

---
+++ server/src/controllers/ItemController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { Item } from '../database/entity/Item';
+
+import database from '../database';
+
+class ItemController {
+  async index(req: Request, res: Response) {
+    const items = await database.then((connection) => connection.manager.find(Item));
+
+    const serializedItems = items.map((item) => ({
+      title: item.title,
+      image_url: `http://127.0.0.1:3333/uploads/${item.image}`,
+    }));
+
+    return res.json(serializedItems);
+  }
+}
+
+export default new ItemController();
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,9 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 
-import database from './database';
-import { Item } from './database/entity/Item';
+import ItemController from './controllers/ItemController';
 
 const routes = Router();
 
-routes.get('/', async (req, res) => {
-  const items = await database.then((connection) => connection.manager.find(Item));
-
-  return res.json(items);
-});
+routes.get('/items', ItemController.index);
 
 export default routes;
