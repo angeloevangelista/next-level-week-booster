56a6fbc8c0dcf3d39885b736f8981cedd263ed4c
angeloevangelista <angeloevan.ane@gmail.com>
2020-06-03 20:24:08 -0300

    Adicionado 'id' da consulta

--- server/src/controllers/ItemController.ts
+++ server/src/controllers/ItemController.ts
@@ -8,6 +8,7 @@ class ItemController {
     const items = await database.then((connection) => connection.manager.find(Item));
 
     const serializedItems = items.map((item) => ({
+      id: item.id,
       title: item.title,
       image_url: `http://127.0.0.1:3333/uploads/${item.image}`,
     }));
