449b5321e5a8fce988e08ff22f0125b44fc7440f
angeloevangelista <angeloevan.ane@gmail.com>
2020-06-03 22:21:41 -0300

    Eslint fix

--- server/src/config/snake_naming.ts
+++ server/src/config/snake_naming.ts
@@ -1,13 +1,20 @@
 import { NamingStrategyInterface, DefaultNamingStrategy } from 'typeorm';
 import { snakeCase } from 'typeorm/util/StringUtils';
 
-export class SnakeNamingStrategy extends DefaultNamingStrategy implements NamingStrategyInterface {
+export class SnakeNamingStrategy
+  extends DefaultNamingStrategy
+  implements NamingStrategyInterface {
   tableName(className: string, customName: string): string {
     return customName || snakeCase(className);
   }
 
-  columnName(propertyName: string, customName: string, embeddedPrefixes: string[]): string {
-    return snakeCase(embeddedPrefixes.join('_')) + (customName || snakeCase(propertyName));
+  columnName(
+    propertyName: string,
+    customName: string,
+    embeddedPrefixes: string[],
+  ): string {
+    return snakeCase(embeddedPrefixes.join('_'))
+    + (customName || snakeCase(propertyName));
   }
 
   relationName(propertyName: string): string {
@@ -22,14 +29,24 @@ export class SnakeNamingStrategy extends DefaultNamingStrategy implements Naming
     secondTableName: string,
     firstPropertyName: string,
     secondPropertyName: string): string {
-    return snakeCase(`${firstTableName}_${firstPropertyName.replace(/\./gi, '_')}_${secondTableName}`);
+    return snakeCase(
+      `${firstTableName}_${firstPropertyName
+        .replace(/\./gi, '_')}_${secondTableName}`,
+    );
   }
 
-  joinTableColumnName(tableName: string, propertyName: string, columnName?: string): string {
+  joinTableColumnName(
+    tableName: string,
+    propertyName: string,
+    columnName?: string,
+  ): string {
     return snakeCase(`${tableName}_${columnName || propertyName}`);
   }
 
-  classTableInheritanceParentColumnName(parentTableName: any, parentTableIdPropertyName: any): string {
+  classTableInheritanceParentColumnName(
+    parentTableName: any,
+    parentTableIdPropertyName: any,
+  ): string {
     return snakeCase(`${parentTableName}_${parentTableIdPropertyName}`);
   }
 }
