ed2a5ecb48ccf1ec54092c12c29790d715232cd3
angeloevangelista <angeloevan.ane@gmail.com>
2020-06-05 18:35:07 -0300

    Carregando cidades

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
@@ -21,15 +21,31 @@ interface IBGEUFResponse {
   sigla: string,
 }
 
+interface IBGECityResponse {
+  id: number,
+  nome: string,
+}
+
 interface UF {
   id: number,
   initial: string,
+}
+
+interface City {
+  id: number,
+  name: string,
 }
 
 const CreatePoint = () => {
   const [items, setItems] = useState<Item[]>([]);
 
   const [ufs, setUfs] = useState<UF[]>([]);
+
+  const [cities, setCities] = useState<City[]>([]);
+
+  const [selectedUf, setSelectedUf] = useState(0);
+
+  const [selectedCity, setSelectedCity] = useState(0);
 
   useEffect(() => {
     apiEcoleta.get('items').then(({ data: items }) => {
@@ -38,15 +54,44 @@ const CreatePoint = () => {
   }, []);
 
   useEffect(() => {
-    apiIBGE.get<IBGEUFResponse[]>('estados?orderBy=nome').then(({ data: ufs }) => {
-      const initials = ufs.map(uf => ({
-        id: uf.id,
-        initial: uf.sigla,
-      }));
+    apiIBGE
+      .get<IBGEUFResponse[]>(
+        'estados?orderBy=nome').then(({ data: ufs }
+        ) => {
+          const initials = ufs.map(uf => ({
+            id: uf.id,
+            initial: uf.sigla,
+          }));
 
-      setUfs(initials);
-    });
-  }, []);
+          setUfs(initials);
+        });
+  }, [selectedUf]);
+
+  useEffect(() => {
+    apiIBGE
+      .get<IBGECityResponse[]>(
+        `estados/${selectedUf}/municipios?orderBy=nome`).then(({ data: citiesResponse }
+        ) => {
+          const cities = citiesResponse.map(city => ({
+            id: city.id,
+            name: city.nome,
+          }));
+
+          setCities(cities);
+        });
+  }, [selectedUf]);
+
+  const handleSelectUF = (e: ChangeEvent<HTMLSelectElement>) => {
+    const { value: uf } = e.target;
+
+    setSelectedUf(Number(uf));
+  }
+
+  const handleSelectCity = (e: ChangeEvent<HTMLSelectElement>) => {
+    const { value: city } = e.target;
+
+    setSelectedCity(Number(city));
+  }
 
   return (
     <div id="page-create-point">
@@ -114,7 +159,12 @@ const CreatePoint = () => {
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
-              <select name="uf" id="uf">
+              <select
+                name="uf"
+                id="uf"
+                value={selectedUf}
+                onChange={handleSelectUF}
+              >
                 <option value="0">Selecione uma UF</option>
 
                 {ufs.map(uf => (
@@ -124,8 +174,18 @@ const CreatePoint = () => {
             </div>
             <div className="field">
               <label htmlFor="city">Cidade</label>
-              <select name="city" id="city">
+              <select
+                name="city"
+                id="city"
+                value={selectedCity}
+                onChange={handleSelectCity}
+                disabled={selectedUf === 0}
+              >
                 <option value="0">Selecione uma cidade</option>
+
+                {cities.map(city => (
+                  <option key={city.id} value={city.id}>{city.name}</option>
+                ))}
               </select>
             </div>
           </div>
