3382f3be0cf84374d3671e91bb6d26dc503abbee
angeloevangelista <angeloevan.ane@gmail.com>
2020-06-06 14:51:46 -0300

    Entendendo diferenças

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Hello World!</Text>
     </View>
   );
 }
@@ -16,4 +16,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 25,
+    fontFamily: 'sans-serif',
+    color: '#7159c1'
+  }
 });
